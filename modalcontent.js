94fcb0c0c4e33b75d1f44dbb4c002aacd17bbdab
hammzaty <71286392+hammzat@users.noreply.github.com>
2022-11-03 20:49:14 +0300

    Add files via upload

---
+++ modalcontent.js
@@ -0,0 +1,141 @@
+var curcontent = new Array();
+
+curcontent["rules"] = {
+	xhead: 'Правила',
+	xcon: '<div class="xbox_custom_rules"><ul><p class="MsoNormal"><span>Основные правила</span></p>\
+<p><li>1.1 Играя на наших серверах, Вы должны ознакомиться с правилами.</li></p>\
+<p><li>1.2 Незнание правил не освобождает от наказания.</li></p>\
+<p><li>1.3 Правила могут дополняться или изменяться в будущем, а под правилами всегда будет дата последнего обновления.</li></p>\
+<p><li>1.4 Модераторы и Администраторы сервера сами выбирают наказание для игрока в зависимости от нарушения и ситуации. Вы можете получить предупреждение, временный бан или вечный бан</li></p>\
+<p><li>1.5 Вы несёте ответственность за все свои аккаунты. Получив бан на старом аккаунте, Вы получите его и на новом.</li></p>\
+<p class="MsoNormal"><span>Правила чата</span></li></p>\
+<p><li>2.1 Запрещено предлагать сделать действия, приводящие к нежеланному выходу с сервера. К таким действиям относится нажатие alt+f4, прописывание disconnect в консоль и прочие подобные действия, о значении которых другие игроки могут не знать.</li></p>\
+<p><li>2.2 Запрещена любая клевета, дезинформация или ложь про игроков, администрацию и сервер.</li></p>\
+<p><li>2.3 Запрещено выдавать себя за администратора, модератора или любого ютубера, если Вы на самом деле им не являетесь.</li></p>\
+<p><li>2.4 Запрещено употребление нецензурных выражений и оскорбления других игроков</li></p>\
+<p><li>2.5 Запрещено проявление расизма в любых формах.</li></p>\
+<p><li>2.6 Запрещена реклама сторонних проектов в любой форме (например в нике - hold#*Тут могла быть ваша реклама*rust)</li></p>\
+<p><li>2.7 Запрещено использование слов "ADMIN" или "MODER" в нике в любом возможном виде.</li></p>\
+<p class="MsoNormal"><span>Игровой процесс</span></li></p>\
+<p><li>3.1 Запрещено использовать/хранить/распространять стороннее программное обеспечение или любые другие средства (читы или макросы), позволяющие получить преимущество над другими игроками. Покупка/подписка приравнивается к хранению.</li></p>\
+<p><li>3.2 Запрещено использование багов игры/сервера с целью или без цели получения преимущества над другими игроками.</li></p>\
+<p><li>3.З Запрещено превышать лимит игроков в команде на серверах.</li></p>\
+<p><li>3.4 Запрещено строиться или хранить лут в камнях, скалах или под фундаментами.</li></p>\
+<p><li>3.5 Запрещено строить "юбку" к дому дальше 10 квадратов в длину (треугольники засчитываются).</li></p>\
+<p><li>3.6 Частая смена тиммейта/тиммейтов карается блокировкой.</li></p><br>\
+<p class="MsoNormal"><span>Дополнительная информация</span></li></p>\
+<p><li>4.1 Если Вас подозревают в читерстве и вызывают на проверку ПК, Вы имеете полное право отказаться от этого, но в этом случае мы будем вынуждены забанить Вас на сервере. Если Вы не читер и хотите продолжить играть у нас на сервере, согласитесь на проверку и выполните действия, которые Вам будет говорить модератор. Мы не будем сохранять или передавать любую информацию, не связанную с растом и читами, которую получим от Вас. Отказ от проверки карается баном на ~3 месяца (90 дней).</li></p>\
+<p><li>4.2 Проверка ПК на наличие читов проводится только в тот момент, когда Вас на неё вызвали, при этом Вы обязаны оставаться на сервере. Если Вы покинули сервер, не предоставили контакты для связи или просите проверить Вас уже после бана, Вам будет отказано и Вы останетесь забанены.</li></p>\
+<p><li>4.3 Администраторы и модераторы имеют право забанить игрока без предупреждения и объяснения, если у них имеются доказательства читерства или нарушения правил.</li></p>\
+<p><li>4.4 Администрация и проверяющие имеют право забанить весь состав друзей/сокланов за игру с читером, за игру с макросами при повторном аккаунте макросника к этим же друзьям, за игру с использованием услуг читеров.</li></p>\
+<p><li>4.5 Администраторы или модераторы не возвращают утерянный лут.</li></p>\
+<p><li>4.6 Вещи и услуги, купленные в магазине, не подлежат возврату, обмену или переносу.</li></p>\
+<p><li>4.7 Мы не компенсируем любые сбои в услугах, которые предоставляются бесплатно.</p>\
+<p><li>4.8 Попытка обмана администрации или модерации сервера карается блокировкой.</p>\
+<p><li>4.9 На проекте есть только администрация и проверяющие (игроки ответственные за проверку на стороннее ПО).</p>\
+<p><li>4.10 Администрация не несёт ответственности за обмены на сервере.</p>\
+<p><li>4.11 Администрация вправе заблокировать вас за неоглашенные правила.</p>\<br>\
+</ul><div class="unban" style="background: white; border-radius: 10px; padding: 20px;"><p class="MsoNormal" style="font-weight: bold; margin-bottom: 5px;">Возможность разблокировки</p><span>Через месяц после блокировки можно подать апелляцию, но, только в случае если бан был получен не за читы или макросы. Писать апелляцию нужно в личные сообщения группы: <a href="https://vk.com/smallestrust">vk.com/smallestrust</a></span></div></div></div>'
+};
+
+function Open(el, usefade = true, zind = false) {
+	closepage();
+	var div1 = document.createElement("div");
+    div1.id = 'Modal';
+	var div2 = document.createElement("div");
+	div2.className = 'modal modal-service fade';
+	if(!usefade) div2.classList.add("show");
+	div2.style = 'display: block; z-index: 1100;';
+	div2.id = 'closer';
+	//div2.onclick = closepage;
+	var div3 = document.createElement("div");
+	div3.className = "modal-dialog modal-lg";
+	if(el=="block")div3.className+=" modal-lg-block"
+	var div4 = document.createElement("div");
+	div4.className = "modal-content";
+	var div5 = document.createElement("div");
+	div5.className = "modal-header";
+	div5.innerHTML = '<h4 class="modal-title">'+curcontent[el].xhead+'</h4>';
+	var div6 = document.createElement("div");
+	div6.className = "modal-body";
+	div6.innerHTML = curcontent[el].xcon;
+	var div7 = document.createElement("div");
+	div7.className = "modal-footer";
+	div7.innerHTML = '<button type="button" class="btn btn-secondary" id="closer" onclick="closepage()">Закрыть</button>';
+	var div8 = document.createElement("div");
+	div8.className = "modal-backdrop fade show";
+	div8.style = 'z-index: 1050;';
+	
+	div1.appendChild(div2);
+	//div2.appendChild(div2_5);
+	div2.appendChild(div3);
+	div3.appendChild(div4);
+	div4.appendChild(div5);
+	div4.appendChild(div6);
+	div4.appendChild(div7);
+	div1.appendChild(div8);
+	if(usefade) setTimeout(()=> div2.classList.add("show"), 0);
+	
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(div1);
+	body.className = "modal-open";
+}
+
+
+function closepage() {
+  var Modal = document.getElementById('Modal');
+  var Modalparent = null;
+  try {
+    Modalparent = ((Modal.parentElement) ? Modal.parentElement : ((Modal.parentNode) ? Modal.parentNode : null));
+  } catch (error) {
+    return;
+  }
+  if (Modalparent == null) return;
+  Modalparent.removeChild(Modal);
+  document.getElementsByTagName('body')[0].className = "";
+}
+
+function search(e){
+	if (e.offsetX < 1) { 
+		console.log(e.target.innerText + ' | ' + e.clientX);
+		if (document.selection) { // IE
+			var range = document.body.createTextRange();
+			range.moveToElementText(e.target);
+			range.select();
+		} else if (window.getSelection) {
+			var range = document.createRange();
+			range.selectNode(e.target);
+			window.getSelection().removeAllRanges();
+			window.getSelection().addRange(range);
+		}
+		try {
+			var successful = document.execCommand('copy');
+			var msg = successful ? 'successful' : 'unsuccessful';
+			console.log('успешно скопирован, нажмите CTRL + V чтобы вставить в консоль F1 в игре.'); 
+		  } catch (err) {
+			console.log('Oops, unable to copy' + err);
+		  }
+    }
+}
+
+window.onload = function () {
+	document.body.onclick=function(event)
+	{
+		if(event.target.id == 'closer')closepage();
+		if(event.target.className == 'MsoCommand')search(event);
+	}
+	
+	for (var i = 0; i < BlockListArrays.length; i++) {
+		curcontent["block"].xcon += '<div class="block_by_category block_category_'+ (i + 1) +'">';
+		for (var b = 0; b < BlockListArrays[i].length; b++) {
+			if(b == 5 && i == 4)
+			curcontent["block"].xcon += '<div class="kit-items__item"><div class="kit-item"><img class="kit-item__image" src="https://moscow-cdn.ru/rust/items/icons256/metal.facemask.png">\
+										<div class="kit-item__quantity block-lvl-'+ (i + 1) +'"></div></div></div>';
+			else
+			curcontent["block"].xcon += '<div class="kit-items__item"><div class="kit-item"><img class="kit-item__image" src="https://static.moscow.ovh/images/games/rust/icons/'+ BlockListArrays[i][b] +'.png">\
+										<div class="kit-item__quantity block-lvl-'+ (i + 1) +'"></div></div></div>';
+		}
+		curcontent["block"].xcon += '</div>';
+	}
+	curcontent["block"].xcon += '</div>\<br></i></div>';
+}
